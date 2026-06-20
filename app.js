5037f879b750d4ec647cc1ecf5af0b38650b067c
Anthony12323 <abenitez2@ufl.edu>
2026-06-20 01:10:13 -0400

    Add timer update function and button functionality

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-let countdown;
+const start = document.getElementById("start");
+const pause = document.getElementById("pause");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
+
+let timeLeft = 5;
+let interval;
+
+const updateTimer = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+
+    timer.innerHTML = 
+    `${minutes.toString().padStart(2, "0")}
+    :
+    ${seconds.toString().padStart(2, "0")}`;
+}
+
+const startTimer = () => {
+    if(interval) { clearInterval(interval); }
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+
+        if(timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 1500;
+            updateTimer();
+        }
+    }, 1000)
+}
+
+const pauseTimer = () => clearInterval(interval);
+
+const resetTimer = () => {
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+start.addEventListener("click", startTimer);
+pause.addEventListener("click", pauseTimer);
+reset.addEventListener("click", resetTimer);
